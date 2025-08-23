93d47ae4011135514ca5e1f365ccecf9363ff7ae
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2025-08-23 14:57:48 +0530

    Delete server directory

--- server/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { saveContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", saveContact);
-
-export default router;
